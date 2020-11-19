f03e0a1e56c2ebebfae543f3c46b9f5297639ce3
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-11-20 10:58:52 +1100

    [src/app/ml-dashboard/ml-dashboard.module.ts] Remove import

--- src/app/ml-dashboard/ml-dashboard.module.ts
+++ src/app/ml-dashboard/ml-dashboard.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { PyStatsModule } from '../py-stats/py-stats.module';
+// import { PyStatsModule } from '../py-stats/py-stats.module';
 import { MlDashboardComponent } from './ml-dashboard.component';
 import { mlDashboardRoutes } from './ml-dashboard.routes';
 
@@ -12,7 +12,7 @@ import { mlDashboardRoutes } from './ml-dashboard.routes';
   imports: [
     CommonModule, RouterModule, RouterModule.forChild(mlDashboardRoutes),
 
-    PyStatsModule
+    // PyStatsModule
   ],
   providers: []
 })
